03ee9609cde7885fe5b60ef8f810e0fea8a30a44
spheredmantis57 <81576411+spheredmantis57@users.noreply.github.com>
2023-03-18 15:24:29 -0400

    Comments

--- retirement/_retirement.py
+++ retirement/_retirement.py
@@ -1,4 +1,6 @@
 class Retirement:
+    """Retirement class allows for general investing/retirement functions
+    """
     RETIREMENT_PRINCIPLE_KEY = "Principle"
     RETIREMENT_YEAR_PAY_KEY = "Withdraw"
     RETIREMENT_END_BAL_KEY = "End"
@@ -10,11 +12,32 @@ class Retirement:
     PRE_EPRINCIPLE__KEY = "pre_end_principle"
 
     def calc_future_dollars(todays_dollars, inflation, years):
+        """calculates the future dollars due to inflation
+
+        Args:
+            float:todays_dollars - amount to calculate future dollars from
+            float:inflation - inflation rate
+            int:years - number of years to simulate inflation
+        
+        Returns:
+            float:future_dollars - dollars calculated with inflation
+        """
+        future_dollars = todays_dollars
         for _ in range(years):
-            todays_dollars *= inflation + 1
-        return todays_dollars
+            future_dollars *= inflation + 1
+        return future_dollars
 
     def calc_investment(principle, growth, montly_invest, years):
+        """create an amortization of an investment
+
+        Args:
+            float:principle - the starting balance
+            float:growth - the APY
+            int:years - the number of years the investment will grow
+
+        Returns:
+            list:the amortization chart
+        """
         running_balance = principle
         actual_principle = running_balance
         amortization = list()
@@ -31,16 +54,48 @@ class Retirement:
         return amortization
 
     def print_retire_entry(year, principle, withdraw, end):
+        """prints a year of the retirement amortization
+
+        Args:
+            int:year - the year of the amortization entry
+            float:principle - the amount of money left in that year
+            float:withdraw - the amount taken out that year in retirement
+            float:end - the money left that year after withdrawing
+        """
         fmt = "{0: >20}"
         year_fmt = "{0: ^6}"
         print(f"{year_fmt.format(year)} {fmt.format(principle)} {fmt.format(withdraw)} {fmt.format(end)}")
     
     def print_preretire_entry(year, sprin, sbal, interest, ebal, eprin):
+        """prints a year of an investment
+
+        Args:
+            int:year - the year of the amortization entry
+            float:sprin - the starting personally invested
+            float:sbal - the starting total amount
+            float:interest - the APY of the investment
+            float:ebal - the ending total amount
+            float:eprin - the ending personally invested
+        """
         fmt = "{0: >20}"
         year_fmt = "{0: ^6}"
         print(f"{year_fmt.format(year)} {fmt.format(sprin)} {fmt.format(sbal)} {fmt.format(interest)} {fmt.format(ebal)} {fmt.format(eprin)}")
 
     def simulate_retirement(principle, initial_pay, growth, inflation, social_security=0):
+        """creates an amortization of retirement years till money runs out
+
+        Args:
+            float:principle - the amount owned at retirement
+            float:initial_pay - (todays dollars) amount to use each year in
+                retirement (will go up each year with inflation)
+            float:growth - APY of the investment
+            float:inflation - percent of inflation
+            float:social_security - (todays dollars) amount of social security
+                received
+
+        Returns:
+            list:the amortization chart (dict for each year)
+        """
         amortization = list()
         for _ in range(80):
             temp_dict = {Retirement.RETIREMENT_PRINCIPLE_KEY: principle, Retirement.RETIREMENT_YEAR_PAY_KEY: initial_pay}
@@ -57,18 +112,47 @@ class Retirement:
         return amortization
 
     def print_retirement_amortization(amortization, age=0):
+        """prints each year of retirement
+
+        Args:
+            list:amortization - elements are dict of each year
+            int:age - the age of the person for the year
+        """
         fmt = "${:.2f}"
         Retirement.print_retire_entry("Year", Retirement.RETIREMENT_PRINCIPLE_KEY, Retirement.RETIREMENT_YEAR_PAY_KEY, Retirement.RETIREMENT_END_BAL_KEY)
         for year, info in enumerate(amortization):
             Retirement.print_retire_entry(year+age, fmt.format(info[Retirement.RETIREMENT_PRINCIPLE_KEY]), fmt.format(info[Retirement.RETIREMENT_YEAR_PAY_KEY]), fmt.format(info[Retirement.RETIREMENT_END_BAL_KEY]))
 
     def print_preretire_amortization(amortization, age=0):
+        """prints each year of preretirement
+
+        Args:
+            list:amortization - elements are dict of each year
+            int:age - the age of the person for the year
+        """
         fmt = "${:.2f}"
         Retirement.print_preretire_entry("Year", "Start Principle", "Start Balance", "Interest", "End Balance", "End Prinicple")
         for year, info in enumerate(amortization):
             Retirement.print_preretire_entry(year+age, fmt.format(info[Retirement.PRE_SPRINCIPLE_KEY]), fmt.format(info[Retirement.PRE_SBALANCE_KEY]), fmt.format(info[Retirement.PRE_INTEREST_KEY]), fmt.format(info[Retirement.PRE_EBALANCE_KEY]), fmt.format(info[Retirement.PRE_EPRINCIPLE__KEY]))
 
     def the_works(years_till_retire, inflation, monthly_invested, principle, yearly_apy_preretire, first_year_selfpay, yearly_apy_retire, social_security=0):
+        """gets both the preretirement and retirement amortization
+
+        Args:
+            int:years_till_retire - number of years till retirement
+            float:inflation - percent of inflation
+            float:monthly_invested - the dollars invested each month
+            float:principle - the amount of money you are starting with
+            float:yearly_apy_preretire - APY before retiring
+            float:first_year_selfpay - (todays dollars) amount to use each year in
+                retirement (will go up each year with inflation)
+            float:yearly_apy_retire - APY in retirement
+            float:social_security - the amount of social security
+        
+        Returns:
+            list:the amortization chart (dict for each year) before retirement
+            list:the amortization chart (dict for each year) during retirement
+        """
         # make year pay future dollars
         first_year_selfpay_adjusted = Retirement.calc_future_dollars(first_year_selfpay, inflation, years_till_retire)
 
--- user_input/_user_input.py
+++ user_input/_user_input.py
@@ -1,9 +1,18 @@
 from os import name, system
 
 class InputHandler:
+    """Class to handle user input
+    """
     WARNING = "Warning: {}"
 
     def __init__(self, allow_cancel=False):
+        """initializes an InputHandler object
+
+        Args:
+            bool:allow_cancel - True if there is an option to cancel input but
+                not quit program; False if when trying to cancel input, the
+                program will quit
+        """
         self.allow_cancel = allow_cancel
         
 
@@ -17,6 +26,8 @@ class InputHandler:
 
     @staticmethod
     def clear():
+        """clears the terminal
+        """
         if name == 'nt':
             system("cls")
         else:
@@ -24,10 +35,15 @@ class InputHandler:
 
 
     def _quit(self):
+        """Handles the user wanting to cancel input or quit the program
+        """
         self.clear()
         print("Press Ctrl+C to confirm QUIT.")
         print("Press ENTER to go back to input screen.")
+
         if self.allow_cancel is True:
+            # allow a user to cancel the cancel, quit, or cancel the input but
+            # not quit the program
             print("Press Ctrl+D to cancel input, but not quit program.")
             try:
                 input()
@@ -35,25 +51,33 @@ class InputHandler:
                 raise InputHandler.QuitProgram("User quitting")
             except EOFError:
                 raise InputHandler.CancelInput("User does not want to enter input")
+
         else:
+            # allow a user to quit the program, or cancel and go back to input
             try:
                 input()
             except (KeyboardInterrupt, EOFError):
                 raise InputHandler.QuitProgram("User quitting")
 
-        
 
     def input_int(self, min=0, max=1000000, input_type="int"):
-        """
-        todo: finish this doc string
-        RETURNS:
-        int:value entered, within range (cannot be trusted if exception raised)
+        """gets an integer from the user
 
-        RAISES:
-        InputHandler.QuitProgram:user wants to quit program
-        InputHandler.CancelQuit:user wants to cancel input, but not quit program
+        Args:
+            int:min - minimum the input can be
+            int:max - maximum the input can be
+            string:input_type - what the int is for (eg: "years")
+
+        Returns:
+            int:value entered, within range (cannot be trusted if exception raised)
+
+        Raises:
+            InputHandler.QuitProgram:user wants to quit program
+            InputHandler.CancelQuit:user wants to cancel input, but not quit program
         """
+        # get the input
         prompt = f"Enter {input_type} (an int from {min} to {max}): "
+        # try again till min and max are met
         while True:
             try:
                 number = input(prompt)
