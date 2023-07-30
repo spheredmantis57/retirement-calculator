c0b83cb487ee7170f0dd22d4a73fa08f320e87a3
spheredmantis57 <81576411+spheredmantis57@users.noreply.github.com>
2023-07-30 00:35:47 -0400

    allow for hosting as well

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # retirement-calculator
 python3 main.py
+
+# todo
+- lint
+- document that running from the main.py will allow cli or a flask server (for development purposes)
+- for actual hosting use gunicorn wsgi:app
--- main.py
+++ main.py
@@ -2,11 +2,46 @@
 """
 # Dowling: only two over
 # pylint: disable=too-many-locals
+from argparse import ArgumentParser
+
 from user_input import InputHandler
 from retirement import Retirement
 
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 SS_LINK = "https://www.ssa.gov/OACT/quickcalc/"
+
+def get_amortizations(years_till_retire, first_year_selfpay, inflation, principle, yearly_apy_preretire, monthly, yearly_apy_retire, social_security):
+    # make year pay future dollars
+    first_year_selfpay_adjusted = \
+        Retirement.calc_future_dollars(first_year_selfpay,
+                                       inflation,
+                                       years_till_retire)
+
+    # calc invested by retire
+    preretire_amortization = \
+        Retirement.calc_investment(principle,
+                                   yearly_apy_preretire,
+                                   monthly,
+                                   years_till_retire)
+    invested_at_retirement = \
+        (preretire_amortization[-1])[Retirement.PRE_EBALANCE_KEY]
+
+    # calc future social_security
+    if social_security != 0:
+        social_security = Retirement.calc_future_dollars(social_security,
+                                                         inflation,
+                                                         years_till_retire)
+
+    # simulate years in retirement
+    retire_amortization = \
+        Retirement.simulate_retirement(invested_at_retirement,
+                                       first_year_selfpay_adjusted,
+                                       yearly_apy_retire, inflation,
+                                       social_security)
+
+    return preretire_amortization, retire_amortization
 
 
 def main():
@@ -40,44 +75,7 @@ def main():
     # simulate retirement
     years_till_retire = retirement_age - age
 
-    # make year pay future dollars
-    first_year_selfpay_adjusted = \
-        Retirement.calc_future_dollars(first_year_selfpay,
-                                       inflation,
-                                       years_till_retire)
-
-    # calc invested by retire
-    preretire_amortization = \
-        Retirement.calc_investment(principle,
-                                   yearly_apy_preretire,
-                                   monthly,
-                                   years_till_retire)
-    invested_at_retirement = \
-        (preretire_amortization[-1])[Retirement.PRE_EBALANCE_KEY]
-
-    # calc future social_security
-    if social_security != 0:
-        social_security = Retirement.calc_future_dollars(social_security,
-                                                         inflation,
-                                                         years_till_retire)
-
-    # simulate years in retirement
-    retire_amortization = \
-        Retirement.simulate_retirement(invested_at_retirement,
-                                       first_year_selfpay_adjusted,
-                                       yearly_apy_retire, inflation,
-                                       social_security)
-
-
-    # preretire_amortization, retire_amortization = \
-    #     Retirement.the_works(
-    #         years_till_retire,
-    #         inflation, monthly,
-    #         principle, yearly_apy_preretire,
-    #         first_year_selfpay,
-    #         yearly_apy_retire,
-    #         social_security
-    #         )
+    preretire_amortization, retire_amortization = get_amortizations(years_till_retire, first_year_selfpay, inflation, principle, yearly_apy_preretire, monthly, yearly_apy_retire, social_security)
 
     # show preretirement amortization
     InputHandler.clear()
@@ -96,5 +94,38 @@ def main():
     Retirement.print_retirement_amortization(retire_amortization,
                                              retirement_age)
 
+@app.route('/')
+def retirement_calculator():
+    return render_template('calculator.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate_retirement():
+    # input from the form
+    current_age = int(request.form['current_age'])
+    retirement_age = int(request.form['retirement_age'])
+    inflation = float(request.form['inflation']) / 100
+    principle = float(request.form['initial_principle'])
+    monthly = float(request.form['monthly_investing'])
+    pre_retire_apy = float(request.form['pre_retire_apy']) / 100
+    first_year_selfpay = float(request.form['first_retirement_distribution'])
+    post_retire_apy = float(request.form['post_retire_apy']) / 100
+    social_security = float(request.form['estimated_monthly_social_security']) * 12
+    # calculate
+    # simulate retirement
+    years_till_retire = retirement_age - current_age
+    preretire_amortization, retire_amortization = get_amortizations(years_till_retire, first_year_selfpay, inflation, principle, pre_retire_apy, monthly, post_retire_apy, social_security)
+    # display results
+    return render_template('results.html',
+                           preretire_amortization=preretire_amortization,
+                           retire_amortization=retire_amortization,
+                           age=current_age,
+                           retirement_age=retirement_age)
+
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser(description="Sample argparse script")
+    parser.add_argument("--cli", action="store_true", help="Set this flag to enable the CLI mode")
+    args = parser.parse_args()
+    if args.cli:
+        main()
+    else:
+        app.run()
